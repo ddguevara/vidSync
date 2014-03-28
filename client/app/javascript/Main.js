2dd89c7b55a8ccc3a4c6307e4695791ea05dedd3
Jasmine Kent <jk@codeartemis.com>
2014-03-29 00:30:43 +0100

    stuff

--- client/app/javascript/Main.js
+++ client/app/javascript/Main.js
@@ -4,6 +4,10 @@ var
 
 Main.onLoad = function () {
   widgetAPI.sendReadyEvent();
+
+  var socket = io.connect('http://188.226.203.5');
+
+  document.getElementById('debug').textContent = 'it works! ' + socket.toString();
 };
 
 Main.onUnload = function () {
