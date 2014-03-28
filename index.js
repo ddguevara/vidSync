296df77ca2624e8c18a197d1d4997779b72350b0
Jasmine Kent <jk@codeartemis.com>
2014-03-28 23:12:08 +0100

    add static and tv endpoint

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.get("/", function(req, res){
     res.render("page");
 });
+app.get("/tv", function(req, res){
+    res.render("tv");
+});
 
- 
+
 app.listen(port);
 console.log("Listening on port " + port);
