fcbb6024f79b58e35976878c94e47fbdb37fab00
Jasmine Kent <jk@codeartemis.com>
2014-03-29 01:16:12 +0100

    cleanup

--- index.js
+++ index.js
@@ -23,17 +23,13 @@ io.sockets.on('connection', function (socket) {
 
   var isMaster = function() {
     var clients = io.sockets.clients();
-    console.log(clients);
     return socket === io.sockets.clients()[0];
   }
 
   socket.emit('chatmessage', { message: 'welcome to the chat' });
   socket.on('chatmessage', function (data) {
-    io.sockets.emit('chatmessage', data);
+    socket.broadcast.emit('chatmessage', data);
   });
-  // socket.on('message', function(data) {
-  //   console.log(data);
-  // });
   socket.on('currentTime', function(data) {
     if (isMaster()) {
       console.log('broadcasting time ' + data.currentTime);
